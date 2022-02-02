13d75024045a34a4642c5658573f56d922af3f23
rhenardianti <45452249+rhenardianti@users.noreply.github.com>
2022-02-02 16:20:12 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-const product = 'Socks'
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         product: 'Socks',
+//         description: 'Made with love by Ulala comapny using the best cotton in the world',
+//         image: './assets/images/socks_green.jpg',
+//         alt: 'Picture of green socks'
+//     }
+// })
+
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
+            product: 'Socks',
+            description: 'Made with love by Ulala comapny using the best cotton in the world',
+            image: './assets/images/socks_green.jpg',
+            alt: 'Picture of green socks',
+            href: 'https://www.vuemastery.com/',
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S','M','L'],
+            variants: [
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            ]
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        }
+    }
+})
